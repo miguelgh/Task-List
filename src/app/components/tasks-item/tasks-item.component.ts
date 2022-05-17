3d51b71db0e87980e3b01bb27d48cd6e53c09469
miguelgh16 <miguelgh16@gmail.com>
2022-05-17 00:15:11 -0300

    se crea componente de lista de items

--- src/app/components/tasks-item/tasks-item.component.ts
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -4,6 +4,9 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Task } from 'src/app/task';
 import { TASKS } from 'src/app/mock-task';
 
+//se carga los íconos de font awesome
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-tasks-item',
   templateUrl: './tasks-item.component.html',
@@ -13,6 +16,9 @@ export class TasksItemComponent implements OnInit {
 //se inicializa el input con el array de la base de datos TASKS en la casilla cero
   @Input() task: Task = TASKS[0];
   constructor() { }
+
+  //se carga el ícono de font awesome
+  faTimes= faTimes;
 
   ngOnInit(): void {
   }
